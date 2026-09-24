92880c60b190530da19b0e834d595a74f6fb3d39
fayaflava-maker <fayaflava@gmail.com>
2026-09-24 12:52:50 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='damien-recommandations-v15-1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/damien-royez.jpg']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='damien-recommandations-v15-2';
+self.addEventListener('install',e=>e.waitUntil((async()=>{await caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/damien-royez.jpg']));await self.skipWaiting();})()));
+self.addEventListener('activate',e=>e.waitUntil((async()=>{await caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))));await self.clients.claim();})()));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
