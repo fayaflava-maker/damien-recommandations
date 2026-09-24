dd826f413d458a4e6b230799e2a9d69b5a468f51
fayaflava-maker <fayaflava@gmail.com>
2026-09-24 14:35:43 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='damien-recommandations-v15-4';
+const CACHE='damien-recommandations-v16';
 const STATIC=['/manifest.webmanifest','/damien-royez.jpg'];
 
 self.addEventListener('install',event=>{
