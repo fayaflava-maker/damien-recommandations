14b2a5733d832a40833f6e18f58db9b569348b6f
fayaflava-maker <fayaflava@gmail.com>
2026-09-25 15:03:39 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## V16.4
+## V16.5
 - Nouvelle icône Damien Recommandations pour iOS et Android/PWA.
 - Nom court écran d’accueil : Damien Reco.
 - Icônes 180, 192 et 512 px + maskable Android.
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='damien-recommandations-v16-4';
+const CACHE='damien-recommandations-v16-5';
 const STATIC=['/manifest.webmanifest','/damien-royez.jpg','/favicon.png','/icons/apple-touch-icon.png','/icons/icon-192.png','/icons/icon-512.png','/icons/icon-maskable-512.png'];
 
 self.addEventListener('install',event=>{
